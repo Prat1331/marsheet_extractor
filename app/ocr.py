98e5c9cac10faa950e4a4bab0b1e0c7afab84663
Prat1331 <Prat1331@users.noreply.github.com>
2026-01-22 22:13:56 +0530

    hii

--- app/ocr.py
+++ app/ocr.py
@@ -1,21 +1,14 @@
 # ocr.py
 import cv2
-import pytesseract
-import tempfile
-from PIL import Image
+import easyocr
+
+reader = easyocr.Reader(['en'], gpu=False)
 
 def extract_text(image_path: str) -> str:
     img = cv2.imread(image_path)
-
     img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 9, 75, 75)
-    thresh = cv2.threshold(
-        gray, 0, 255,
-        cv2.THRESH_BINARY + cv2.THRESH_OTSU
-    )[1]
-    text = pytesseract.image_to_string(
-        Image.fromarray(thresh),
-        config="--psm 6"
-    )
+
+    results = reader.readtext(img)
+    text = "\n".join([r[1] for r in results])
+
     return text
